d3f2511a4de4d164755f5b1b5d04de81377543ba
Alex Sandro Belfort Corrêa <alex.sandro_as@hotmail.com>
2023-03-22 15:42:58 -0300

    base url

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const { token } = JSON.parse(localStorage.getItem("userTD")) || "";
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API,
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     Authorization: `Bearer ${token}`,
     //Authorization: `Bearer ${localStorage.getItem("access_token")}`,
